14cf2fbea53521a11cd0de72afda1a4d877a9169
VikramMoger1 <Vikram.Moger@ascenddefi.com>
2024-10-29 13:42:43 +0530

    updated fiatswap backend

---
+++ backend/Swap/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from Swap import views
+from .views import get_currency_icon,get_all_currency_icons,get_fiat_wallet_by_user_id,convert_currency,AccountTypeList
+
+urlpatterns = [
+    path('get-currency-icon/', get_currency_icon, name='get-currency-icon'),
+    path('icons/', get_all_currency_icons, name='get_all_currency_icons'),
+    path('fiat_wallet/<str:wallet_id>/', get_fiat_wallet_by_user_id, name='get_fiat_wallet_by_user_id'),
+    path('convert_currency/', convert_currency, name='convert_currency'),
+     path('account-types/', AccountTypeList.as_view(), name='account-type-list'),
+]
